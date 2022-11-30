33d7b72265ebe9138ee05aa47c4fb43b98bdf872
Jin <novhunter@gmail.com>
2022-11-29 17:55:29 -0700

    Assigned color attributes to three objects

---
+++ Vehicle.py
@@ -0,0 +1,10 @@
+class Vehicle:
+    pass
+
+bug_object = Vehicle()
+turtle_object = Vehicle()
+rover_object = Vehicle()
+
+bug_object.color = "yellow"
+turtle_object.color = "green"
+rover_object.color = "purple"
