12817bcb3fd5ddbe40c1d96333ec67d31aa166b0
Jin <novhunter@gmail.com>
2022-11-29 18:02:50 -0700

    calling and displaying object attributes

--- Vehicle.py
+++ Vehicle.py
@@ -1,10 +1,22 @@
 class Vehicle:
     pass
 
-bug_object = Vehicle()
+bug_object = Vehicle() #object of the vehicle class and an instance of the vehicle class
 turtle_object = Vehicle()
 rover_object = Vehicle()
 
 bug_object.color = "yellow"
+bug_object.num_wheels = 4
+bug_object.speed = 1
+
 turtle_object.color = "green"
+turtle_object.num_wheels = 2
+turtle_object.speed = 5
+
 rover_object.color = "purple"
+rover_object.num_wheels = 4
+rover_object.speed = 25
+
+print(bug_object.speed)
+print(turtle_object.speed)
+print(rover_object.speed)
