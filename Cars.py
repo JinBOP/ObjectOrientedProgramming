d21dd26d03b3553e43efa0f4a4edc4f43f88264c
Jin <novhunter@gmail.com>
2022-11-29 18:45:39 -0700

    imported data from object to main function file

--- Cars.py
+++ Cars.py
@@ -1,2 +1,13 @@
 # import info from class
 from Vehicle import Vehicle
+
+bug_object = Vehicle("Beetle", "Yellow", 4, 1) #object of the vehicle class and an instance of the vehicle class
+turtle_object = Vehicle("Turtle", "Green", 2, 5)
+rover_object = Vehicle("Rover", "Purple", 4, 25)
+
+bug_object.print_details()
+# turtle_object.print_details()
+# rover_object.print_details()
+
+bug_object.paint_vehicle("blue")
+bug_object.print_details()
--- Vehicle.py
+++ Vehicle.py
@@ -17,14 +17,3 @@ class Vehicle:
 
     def paint_vehicle(self, new_color):
         self.color = new_color
-        
-bug_object = Vehicle("Beetle", "Yellow", 4, 1) #object of the vehicle class and an instance of the vehicle class
-turtle_object = Vehicle("Turtle", "Green", 2, 5)
-rover_object = Vehicle("Rover", "Purple", 4, 25)
-
-bug_object.print_details()
-# turtle_object.print_details()
-# rover_object.print_details()
-
-bug_object.paint_vehicle("blue")
-bug_object.print_details()
